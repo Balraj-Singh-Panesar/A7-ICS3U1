668c2dbb38a896044b76e4ffcbaf5e2bd62b386f
Balraj Singh <balrajkhalsa13@gmail.com>
2024-01-11 11:29:01 -0500

    Initialized main variables and Array in ConnectFour.java

--- src/A7.java
+++ src/A7.java
@@ -1,4 +1,15 @@
-
+/*
+* Culminating Assignment A7 in ICS3U1
+* 2024/01/11
+* Code Contributors: Balraj Singh,
+* ROLES:
+* Chief Operating Officer - Charlie
+* Product Manager - Isa
+* Coder - Balraj
+* User Experience - Kristiana
+* Quality Assurance - Ella
+*
+* */
 class A7 {
     public static void main(String[] args) {
         System.out.println("Hello World");
--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,3 +1,24 @@
+import javax.swing.*;
 class ConnectFour{
+    //Keeps track of what player is currently playing, if the player is player one it is true
+    private boolean isPlayerOne;
+    //Keeps track of if a column is full to prevent a player for adding a piece into full column
+    private boolean columnIsFull;
+    //
+    private enum matchState {
+      WON, TIE, PLAYING
+    };
+
+    private int [][] board = {{1, 2, 3, 4, 5, 6, 7}, {1, 2, 3, 4, 5, 6, 7}};
+    private static void drawBoard(){
+
+    }
+
+
+
+
+
+
+
 
 }

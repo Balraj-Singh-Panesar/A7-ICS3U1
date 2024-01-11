fe1c3fe6df49e9c5f2443d712d35b0135eaea957
charlie <cmarino3299@hwdsb.on.ca>
2024-01-11 11:58:55 -0500

    test

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,3 +1,15 @@
-class MainMenu{
+import java.util.Scanner;
 
+class MainMenu{
+    public static void menu() {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Main Menu \n====================================");
+        System.out.println("1. Connect 4 \n2. Quit");
+        int choice = myObj.nextInt();
+
+        switch (choice) {
+            case 1:
+                // connect 4 functions
+        }
+    }
 }

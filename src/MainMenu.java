e89907388889177a53e22f35d7e6a5f7d924a744
charlie <cmarino3299@hwdsb.on.ca>
2024-01-15 12:01:09 -0500

    Added a JFrame for the main menu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,15 +1,44 @@
 import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
 class MainMenu{
+
+    String playerOneName = "Player One";
+    String playerTwoName = "Player Two";
     public static void menu() {
+
+        JFrame myFrame = new JFrame();
+
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setSize(new Dimension(500,500));
+        myFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        myFrame.setLocationRelativeTo(null);
+
+
+
         Scanner myObj = new Scanner(System.in);
+
+        // gets the users names
+        System.out.print("What is player ones name?");
+        String playerOneName = myObj.nextLine();
+        System.out.println("What is player twos name");
+        String playerTwoName = myObj.nextLine();
+
+        /*
         System.out.println("Main Menu \n====================================");
         System.out.println("1. Connect 4 \n2. Quit");
+
+        // the user chooses what they would like to do
         int choice = myObj.nextInt();
 
+        // determines if the user wants to play connect 4 or exit the program
         switch (choice) {
-            case 1:
-                // connect 4 functions k
-        }
+            case 1: // connect 4 is chosen
+                // connect 4 function
+            case 2: // the user exits the game
+                break;
+        } */
     }
 }
+
---
+++ src/TitleScreen.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class TitleScreen {
+    private JPanel TitleScreen;
+    private JTextField connect4TextField;
+    private JButton playConnect4Button;
+    private JTextField playerOneTextField;
+}
